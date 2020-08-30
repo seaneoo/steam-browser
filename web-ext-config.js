8558a79af3d9daabebb80cb2429534c780b44486
Sean O'Connor <seanoconnor941@gmail.com>
2020-08-30 13:45:55 -0600

    Project and config setup

---
+++ web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  build: {
+    overwriteDest: true,
+  },
+  ignoreFiles: [
+    "./src",
+    "package-lock.json",
+    "package.json",
+    "webpack.config.js",
+    "LICENSE",
+    "README.md",
+  ],
+};
