2efe51b061d0140fd60888582dd57cd6da762974
Sean O'Connor <seanoconnor941@gmail.com>
2020-08-30 18:40:49 -0600

    Only open certain pages

--- src/background.js
+++ src/background.js
@@ -1,14 +1,36 @@
 let runtime = typeof chrome !== undefined ? chrome : browser;
+let canOpen = false;
 
-function open() {
-  runtime.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var currentTabUrl = tabs[0].url;
+runtime.browserAction.onClicked.addListener(() => {
+  if (canOpen) {
+    runtime.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var currentTabUrl = tabs[0].url;
 
-    console.log(currentTabUrl);
-    runtime.tabs.create({
-      url: `steam://openurl/${currentTabUrl}`,
+      console.log(currentTabUrl);
+      runtime.tabs.create({
+        url: `steam://openurl/${currentTabUrl}`,
+      });
     });
-  });
-}
+  }
+});
 
-runtime.browserAction.onClicked.addListener(open);
+runtime.tabs.onUpdated.addListener(() => {
+  runtime.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+      url: ["*://*.steamcommunity.com/*", "*://store.steampowered.com/*"],
+    },
+    (tabs) => {
+      canOpen = tabs.length > 0;
+
+      var ico = `steam${canOpen ? "" : "-gray"}`;
+      runtime.browserAction.setIcon({
+        path: {
+          16: `/assets/${ico}16.png`,
+          32: `/assets/${ico}32.png`,
+        },
+      });
+    }
+  );
+});
